163d7139bd47489c2f3f9bcf91441ee6bcaf4052
cybersamx <cybersamx@gmail.com>
2024-08-16 16:56:53 -0700

    Simplify compare function and readme, v1.0.3

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 from dataclasses import dataclass
 
@@ -14,21 +15,25 @@ class StatsTestCase:
     mean_expect: float
 
 
-tcs = [
-    StatsTestCase([1, 2, 3, 4], 4, 2.5),
-    StatsTestCase([610, 450, 160, 420, 310], 5, 390),
-]
+def compare(a: float, b: float) -> bool:
+    epsilon = 0.00001
+    return math.fabs(a-b) < epsilon
 
 
 class TestStats(unittest.TestCase):
+    tcs = [
+        StatsTestCase([1, 2, 3, 4], 4, 2.5),
+        StatsTestCase([610, 450, 160, 420, 310], 5, 390),
+    ]
+
     def test_count(self):
-        for tc in tcs:
+        for tc in self.tcs:
             res = count(tc.m)
             expect = tc.count_expect
             self.assertEqual(res, expect)
 
     def test_mean(self):
-        for tc in tcs:
+        for tc in self.tcs:
             res = mean(tc.m)
             expect = tc.mean_expect
             self.assertEqual(res, expect)
